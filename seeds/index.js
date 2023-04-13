ea7f8672041b28967597e50504b5f66627866df8
waran2337 <waran2337@gmail.com>
2023-04-13 23:15:46 +0530

    adding MAPBOX on Show Page and MAP CLUSTER on index Page

--- seeds/index.js
+++ seeds/index.js
@@ -19,25 +19,26 @@ const sample = array => array[Math.floor(Math.random()  * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i=0; i<50; i++) {
+    for(let i=0; i<300; i++) {
         const random1000 = Math.floor(Math.random()*400);
         // console.log(output[random1000]);
         const camp = new Campground({
-            location: `${output[random1000].city}, ${output[random1000].admin_name}`,
+            location: `${output[random1000].city}, ${output[random1000].country}`,
             // location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'It is a long established fact that a reader will be distracted by the readable ' +
                 'content of a page when looking at its layout. The point of using Lorem ' +
                 'Ipsum is that it has a more-or-less normal distribution of letters,',
             price: random1000,
-            author: '6428707e65b845f2471e6308',
+            author: '6428707e65b845f2471e6308', //your User id called waran
+            geometry: { type: 'Point', coordinates: [ output[random1000].lng, output[random1000].lat  ] },
             images: [
                 {
-                    url: 'https://res.cloudinary.com/dyquncffs/image/upload/v1680959192/YelpCamp/ydzbimrdb4pbquk1gwah.png',
+                    url: 'https://res.cloudinary.com/dyquncffs/image/upload/v1681354863/YelpCamp/rzjcxjhua5nkrlmrmn78.jpg',
                     filename: 'YelpCamp/ydzbimrdb4pbquk1gwah',
                 },
                 {
-                    url: 'https://res.cloudinary.com/dyquncffs/image/upload/v1680959192/YelpCamp/sl9nz63hif4lwtgzfqr6.png',
+                    url: 'https://res.cloudinary.com/dyquncffs/image/upload/v1681354861/YelpCamp/rb84trxubfvmmdi2w0qf.jpg',
                     filename: 'YelpCamp/sl9nz63hif4lwtgzfqr6',
                 }
             ]
